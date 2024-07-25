279c89d0b5a9cc386a1d7b3c38d856b339d4174c
mac <mac@macs-MacBook-Pro.local>
2024-07-25 14:13:46 +0500

    First commit from umar in branch

---
+++ process.php
@@ -0,0 +1,16 @@
+<?php
+    // Enable error reporting for debugging
+    // error_reporting(E_ALL);
+    // ini_set('display_errors', 1);
+  $path = (getenv("MPDF_ROOT")) ? getenv("MPDF_ROOT") : __DIR__;
+//   var_dump($path);die;
+  require_once $path . "/vendor/autoload.php";
+  $pdfcontent = '<table class="form-data"><thead><tr> </tr></thead><tbody>';
+  foreach($_POST as $key =>$value){
+      $pdfcontent .= "<tr><td>" . ucwords(str_replace("_", " ",$key)) . "</td>:<td>" . $value . "</td></tr>";
+  }
+  $pdfcontent .= "</tbody></table>";
+  $mpdf = new \Mpdf\Mpdf();
+  $mpdf->WriteHTML(utf8_encode($pdfcontent));
+  $mpdf->Output('formdata.pdf', 'D');
+?>
